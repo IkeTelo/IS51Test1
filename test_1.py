807fd62ab03cab9bbe1554d506daf6257a44e3ac
Isaac Telloian <iketelloian@fresnostate.edu>
2022-03-24 16:53:49 -0700

    finish exam-end

--- test_1.py
+++ test_1.py
@@ -39,3 +39,37 @@ else
 
 main
 """
+
+def option1():
+    return 100 * 10
+
+def option2():
+    amount = 1
+    list1 = []
+    for i in range(0, 10):
+        list1.append(amount)
+        amount *= 2
+    # print("list1", list1) = is verrification of list working
+    total = sum(list1)
+    return total
+
+def main():
+    answer = ""
+    var1 = option1()  # 100 * 10 = 1000
+    var2 = option2()  # addition of list1 = 1023
+    # print("from main: car1", var1, "var2", var2) = testing
+    if var1 == var2:
+        answer = "Option 1 and Option 2 pay the same!"
+    elif var1 > var2:
+        answer = "Option 1 is better!"
+    else:
+        answer = "Option 2 is better!"
+    print(answer)
+
+# var1 = option1() = testing
+# var2 = option2()
+# print(var1)
+# print(var2)
+
+
+main()
